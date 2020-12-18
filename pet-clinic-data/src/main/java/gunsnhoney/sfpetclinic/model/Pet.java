fdf36969ad6d2a23a299d5a252b18be2eaea331e
Hugh <intelzombi@users.noreply.github.com>
2020-12-18 11:48:26 -0600

    adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package gunsnhoney.sfpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private String name;
     private LocalDate birthDate;
