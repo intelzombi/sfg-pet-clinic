9f0aaa3cbb40a5a5df9f8b016a5446f6d29241f1
Hugh <intelzombi@users.noreply.github.com>
2020-12-21 13:27:54 -0600

    Update Map Impl to manage setting of ID Property. closes #23

--- pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Carl");
         owner1.setLastName("Perkins");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2l);
         owner2.setFirstName("Sara");
         owner2.setLastName("Smith");
 
@@ -39,12 +37,10 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Gary");
         vet1.setLastName("Palaro");
-        vet1.setId(3L);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(4L);
         vet2.setFirstName("Dave");
         vet2.setLastName("Severs");
         vetService.save(vet2);
--- pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/service/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package gunsnhoney.sfpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import gunsnhoney.sfpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<T>(map.values());
@@ -16,8 +15,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -27,5 +33,13 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        try {
+            return Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            return 1L;
+        }
     }
 }
