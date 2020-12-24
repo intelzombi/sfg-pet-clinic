60b989ef11e918318c58c2b8c5a286d2ae5fcc95
Hugh <intelzombi@users.noreply.github.com>
2020-12-23 18:32:05 -0600

    Create Vet Specialty Entity, Associate to Vet. closes #27

---
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package gunsnhoney.sfpetclinic.model;
+
+public class Specialty extends BaseEntity {
+    private String descriptionnn;
+
+    public String getDescriptionnn() {
+        return descriptionnn;
+    }
+
+    public void setDescriptionnn(String descriptionnn) {
+        this.descriptionnn = descriptionnn;
+    }
+}
--- pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package gunsnhoney.sfpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void addSpecialty(Specialty specialty) {
+        this.specialties.add(specialty);
+    }
 }
