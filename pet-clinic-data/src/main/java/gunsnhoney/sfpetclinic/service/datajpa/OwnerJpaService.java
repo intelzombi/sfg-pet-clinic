879b68ca70726f34f397b5fed7e17615309c3543
Hugh <intelzombi@users.noreply.github.com>
2020-12-30 12:16:38 -0600

    Create JPA Data Service for Owners. closes #40

---
+++ pet-clinic-data/src/main/java/gunsnhoney/sfpetclinic/service/datajpa/OwnerJpaService.java
@@ -0,0 +1,58 @@
+package gunsnhoney.sfpetclinic.service.datajpa;
+
+import gunsnhoney.sfpetclinic.model.Owner;
+import gunsnhoney.sfpetclinic.repositories.OwnerRepository;
+import gunsnhoney.sfpetclinic.repositories.PetRepository;
+import gunsnhoney.sfpetclinic.repositories.PetTypeRepository;
+import gunsnhoney.sfpetclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("datajpa")
+public class OwnerJpaService implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
